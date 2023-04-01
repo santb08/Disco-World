ad462feec301420b6875a5912edf91dd58202ccd
Santiago <scb9908@gmail.com>
2023-04-01 04:35:11 -0500

    feat: first commit

---
+++ DiscoWorld/DiscoWorld.cs
@@ -0,0 +1,53 @@
+﻿
+namespace DiscoWorld;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Color backgroundColor;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        int redComponent = gameTime.TotalGameTime.Milliseconds / 4;
+        backgroundColor = new Color(redComponent, 0, 0);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(backgroundColor);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
+
